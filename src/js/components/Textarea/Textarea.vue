373119af3b5f663b0cbe60c4e846d0c867f40889
Serg <64j@mail.ru>
2024-01-22 15:20:30 +0300

    up

--- src/js/components/Textarea/Textarea.vue
+++ src/js/components/Textarea/Textarea.vue
@@ -6,12 +6,19 @@ export default {
   __isStatic: true,
   components: { Actions },
   props: ['key', 'type', 'name', 'title', 'value', 'actions', 'rows'],
+  computed: {
+    model: {
+      set (value) {
+        this.$emit('update:value', value)
+      },
+      get () {
+        return this.value
+      }
+    }
+  },
   methods: {
     action (action) {
       this.$emit('action', action)
-    },
-    updateValue (event) {
-      this.$emit('update:value', event.target.value)
     }
   }
 }
@@ -21,13 +28,7 @@ export default {
   <div class="mf3-item">
     <actions @action="action" :actions="actions"/>
     <div class="mf3-items">
-      <textarea @input="updateValue" :rows="rows">{{ value }}</textarea>
+      <textarea v-model="model" :rows="rows"/>
     </div>
   </div>
 </template>
-
-<style scoped>
-.mf3-item textarea {
-  @apply w-full border block resize-y min-h-7
-}
-</style>
